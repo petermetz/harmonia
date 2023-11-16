ce3b172142a4552594e1736cf296e1299e92ba3b
Peter Somogyvari <peter.somogyvari@accenture.com>
2023-11-15 22:07:15 -0800

    chore: add cacti besu all-in-one ledger to hardhat config
    This new configuration can be used like this:
    
    ```sh
    npx hardhat run deploy.js --network cacti
    ```
    
    Also updated the deploy.js script so that it does the
    initial test token transfers dynamically on all the
    signers instead of hardcoding it to 20 (which fails
    for the Cacti test ledger because its genesis json
    is the default besu one which only ships with 3 accounts)

--- src/r3/atomic-swap/evm/hardhat.config.js
+++ src/r3/atomic-swap/evm/hardhat.config.js
@@ -14,8 +14,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.20",
-  optimizer: {enabled: true},
+  optimizer: { enabled: true },
   networks: {
+    cacti: {
+      url: 'http://127.0.0.1:8545',
+      gas: "auto",
+      gasPrice: "auto",
+      accounts: [
+        `c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3`, 
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', 
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'
+      ],
+      from: "627306090abaB3A6e1400e9345bC60c78a8BEf57"
+    },
     hardhat: {
       mining: {
         mempool: {
